1a2b78ba9491be8729bb078bc62bcf1d2b63dad3
GMMarshall <gmmarshall27@gmail.com>
2021-09-27 18:02:35 -0400

    working on getting current weather function

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 var APIKey = "1c2d55e5ab4bd47916f6287ef9d3e643";
-var city;
+var city="";
 var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=" + APIKey;
+var searchedCity = document.getElementById('searchedCity');
+var searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click',currentWeather);
 
-fetch(queryURL);
+
+/*function displayWeather(event){
+    event.preventDefault();
+    if(searchedCity.val().trim()!==""){
+        city= searchedCity.val().trim();
+        currentWeather(city);
+    }
+}*/
+
+function currentWeather(city){
+    $.ajax({
+        url: queryURL,
+        method:"GET",
+    }).then(function(response){
+        console.log(response);
+
+    });
+}
+

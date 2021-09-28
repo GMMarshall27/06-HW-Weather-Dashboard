1e8edecbd55bd18cc4d1d0fe1a78a98c67a34d4b
GMMarshall <gmmarshall27@gmail.com>
2021-09-28 16:07:46 -0400

    got city name and temp to display for currentWeather

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var APIKey = "1c2d55e5ab4bd47916f6287ef9d3e643";
-
-
+var currentTemp = $('#temp');
+var currentWind = $('#wind');
+var currentHumid = $('#humid');
+var currentUv = $('#uv');
+var currentCity = $('#currentCity');
+var searchedCity = $("#searchedCity");
 var searchButton = $('#searchButton');
 searchButton.on('click',function(){
     
@@ -12,22 +16,28 @@ searchButton.on('click',function(){
 });
 
 
-/*function displayWeather(event){
+function displayWeather(event){
     event.preventDefault();
     if(searchedCity.val().trim()!==""){
         city= searchedCity.val().trim();
         currentWeather(city);
     }
-}*/
+}
 
 function currentWeather(){
     var city = localStorage.getItem("city");
-    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var queryURL= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
     $.ajax({
         url: queryURL,
         method:"GET",
     }).then(function(response){
         console.log(response);
+
+        var weathericon= response.weather[0].icon;
+        var iconURL= "https://openweathermap.org/img/wn/" + weathericon + "2x.png";
+        var currentDate =new Date(response.dt*1000).toLocaleDateString();
+        currentCity.html(response.name +"("+currentDate+")"+ "<img src="+iconURL+">");
+        currentTemp.html(response.main.temp+" F");
 
     });
 }

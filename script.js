981221e49e839572b770ace667c3c9a8f93c00fa
GMMarshall <gmmarshall27@gmail.com>
2021-09-27 20:50:39 -0400

    got function to pull weather data

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var APIKey = "1c2d55e5ab4bd47916f6287ef9d3e643";
-var city="";
-var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=" + APIKey;
-var searchedCity = document.getElementById('searchedCity');
-var searchButton = document.getElementById('searchButton');
-searchButton.addEventListener('click',currentWeather);
+
+
+var searchButton = $('#searchButton');
+searchButton.on('click',function(){
+    
+    var city= $("#searchedCity").val();
+    if(city !== null){
+        localStorage.setItem("city", city)
+    }
+    currentWeather();
+});
 
 
 /*function displayWeather(event){
@@ -14,7 +20,9 @@ searchButton.addEventListener('click',currentWeather);
     }
 }*/
 
-function currentWeather(city){
+function currentWeather(){
+    var city = localStorage.getItem("city");
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     $.ajax({
         url: queryURL,
         method:"GET",

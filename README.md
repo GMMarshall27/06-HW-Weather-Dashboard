c7e4cf899d1d4fd70daedeee54103492ce4e8559
GMMarshall <gmmarshall27@gmail.com>
2021-09-29 09:02:26 -0400

    commented js and updated readMe

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 06-HW-Weather-Dashboard
+Description:
+
+For this assignment I started from scratch to build a weather dashboard using, JS, CSS, Bootstrap, Jquery and API's. I was able to pull the weather data from the current day and 5 day forecast from Openmapweather API. With the data I showed the temp, wind, humidity, and uvi index. The uvi index will also change colors based on the current rating. The dashboard will also store and display all previous searched citites. Below you will find the application link and screenshot. 
+
+Deployed application Link: https://gmmarshall27.github.io/06-HW-Weather-Dashboard/
+
+Screenshot:
+![gmmarshall27 github io_06-HW-Weather-Dashboard_](https://user-images.githubusercontent.com/84820751/135273471-296d43b1-3bf4-46e6-bda9-b9cd1b243cc4.png)
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//declared variables
 var APIKey = "1c2d55e5ab4bd47916f6287ef9d3e643";
 var currentTemp = $('#temp');
 var currentWind = $('#wind');
@@ -10,6 +11,7 @@ var currentUv = $('#uv');
 var currentCity = $('#currentCity');
 var searchedCity = $("#searchedCity");
 var searchButton = $('#searchButton');
+//search button that stores searched city
 searchButton.on('click',function(){
     
     var city= $("#searchedCity").val();
@@ -18,7 +20,7 @@ searchButton.on('click',function(){
     }
     currentWeather();
 });
-
+//function to pull current weather data from API and stores it
 function currentWeather(){
     var city = localStorage.getItem("city");
     var queryURL= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
@@ -28,8 +30,9 @@ function currentWeather(){
     }).then(function(response){
         console.log(response);
         displayWeather(response);
-        //uviColor();
+        
     }); 
+    //function to display current weather city, temp, wind, and humidity
     } function displayWeather(response){
         var weathericon= response.weather[0].icon;
         var iconURL= "http://openweathermap.org/img/wn/" + weathericon + "@2x.png";
@@ -39,6 +42,7 @@ function currentWeather(){
         currentWind.html(response.wind.speed+" MPH");
         currentHumid.html(response.main.humidity+" %");
         console.log(response.coord.lat);
+        //pulls UV index from second api and stores it
         getUV(response.coord.lon,response.coord.lat);
         if(response.cod==200){
             savedCity=JSON.parse(localStorage.getItem('cityname'));
@@ -56,10 +60,10 @@ function currentWeather(){
                     addToList(city);
                 }
             }
-        } //uviColor();
+        } 
     }
 
-
+//function using api to get the uv index based off longitude and latitude
 function getUV(lon,lat) {
     var uvURL="https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" +lon+ "&units=imperial&appid=" + APIKey;
     $.ajax({
@@ -69,6 +73,7 @@ function getUV(lon,lat) {
         console.log(response);
         var todayUV=response.current.uvi;
         currentUv.html(todayUV);
+        //function to change uv background color based on rating number
         function uviColor(){
             if(todayUV<=3){
                 $('#uv').css('background-color', 'green');
@@ -81,6 +86,7 @@ function getUV(lon,lat) {
         } uviColor();
         fiveDay(response);
     });
+    //function to display the 5 day forcescast from API
  function fiveDay(response){
      for (i=1;i<6;i++){
          
@@ -95,12 +101,14 @@ function getUV(lon,lat) {
     }
  }
 }
+//function to add the searched city to a list and display it under search bar.
 function addToList(city){
     var listEl= $("<li>"+city.toUpperCase()+"</li>");
     $(listEl).attr("class","list-group-item");
     $(listEl).attr("data-value",city.toUpperCase());
     $(".list-group").append(listEl);
 }
+//function to load the saved data from the searched city. 
 function loadCity(){
     $('ul').empty();
     var savedCity = JSON.parse(localStorage.getItem("cityname"));

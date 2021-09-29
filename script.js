d55df763681e21139067cd84a33b7aef0e11558a
GMMarshall <gmmarshall27@gmail.com>
2021-09-28 23:24:11 -0400

    finally got storage to stay on page

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var APIKey = "1c2d55e5ab4bd47916f6287ef9d3e643";
 var currentTemp = $('#temp');
 var currentWind = $('#wind');
 var currentHumid = $('#humid');
+var savedCity=[];
 var lon;
 var lat;
+var city;
 var currentUv = $('#uv');
 var currentCity = $('#currentCity');
 var searchedCity = $("#searchedCity");
@@ -25,7 +27,9 @@ function currentWeather(){
         method:"GET",
     }).then(function(response){
         console.log(response);
-
+        displayWeather(response);
+    });
+    } function displayWeather(response){
         var weathericon= response.weather[0].icon;
         var iconURL= "http://openweathermap.org/img/wn/" + weathericon + "@2x.png";
         var currentDate =new Date(response.dt*1000).toLocaleDateString();
@@ -35,9 +39,25 @@ function currentWeather(){
         currentHumid.html(response.main.humidity+" %");
         console.log(response.coord.lat);
         getUV(response.coord.lon,response.coord.lat);
+        if(response.cod==200){
+            savedCity=JSON.parse(localStorage.getItem('cityname'));
+            city=response.name;
+            console.log(city);
+            if(savedCity==null){
+                savedCity=[];
+                savedCity.push(city);
+                localStorage.setItem("cityname",JSON.stringify(savedCity));
+            }
+            else {
+                if(find(city)>0){
+                    savedCity.push(city);
+                    localStorage.setItem("cityname",JSON.stringify(savedCity));
+                    addToList(city);
+                }
+            }
+        }
+    }
 
-    });
-}
 
 function getUV(lon,lat) {
     var uvURL="https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" +lon+ "&units=imperial&appid=" + APIKey;
@@ -62,4 +82,22 @@ function getUV(lon,lat) {
          $('#futureImage'+i).html("<img src="+iconURL+">");
     }
  }
+}
+function addToList(city){
+    var listEl= $("<li>"+city.toUpperCase()+"</li>");
+    $(listEl).attr("class","list-group-item");
+    $(listEl).attr("data-value",city.toUpperCase());
+    $(".list-group").append(listEl);
+}
+function loadCity(){
+    $('ul').empty();
+    var savedCity = JSON.parse(localStorage.getItem("cityname"));
+    if(savedCity !==null){
+        savedCity = JSON.parse(localStorage.getItem('cityname'));
+        for(i=0;i<savedCity.length;i++){
+            addToList(savedCity[i]);
+        }
+        city=savedCity[i-1];
+        currentWeather(city);
+    }
 }
